b0ec5c61d57c84163b22582909a8027aa582902c
1q23lyc45 <174172451+1q23lyc45@users.noreply.github.com>
2024-12-24 22:14:55 +0800

    boot-completed.sh support (#18)

--- native/src/core/bootstages.cpp
+++ native/src/core/bootstages.cpp
@@ -355,6 +355,7 @@ void MagiskD::boot_complete() const {
     as_rust().setup_logfile();
 
     LOGI("** boot-complete triggered\n");
+    exec_module_scripts("boot-completed");
     enable_mount_su();
 
     // At this point it's safe to create the folder
